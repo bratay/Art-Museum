b79ce39f22c16608c540d91ee4fb21287b000712
Branden Taylor <brandentaylor@google.com>
2019-07-30 15:45:58 -0700

    The first page

---
+++ lib/data.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'dart:io';
+
+Future<List<ObjectInfo>> requestImages() async {
+  try {
+    Uri apiUri = Uri.parse(
+        'https://api.harvardartmuseums.org/object?sort=random&apikey=b1e1f600-aa67-11e9-adb9-2fdc07e84b9c');
+
+    HttpClientRequest request = await HttpClient().getUrl(apiUri);
+    HttpClientResponse response = await request.close();
+
+    Stream resStream = response.transform(utf8.decoder);
+    var result = '';
+    await for (var data in resStream) {
+      result += data as String;
+    }
+
+    List<ObjectInfo> output =
+    MuseumResponse.fromJson(json.decode(result) as Map<String, dynamic>)
+        .records
+        .where((r) => r.primaryImageUrl != null)
+        .toList();
+
+    return output;
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
+}
+
+class MuseumResponse {
+  List<ObjectInfo> records;
+
+  MuseumResponse.fromJson(Map<String, dynamic> map) {
+    this.records = (map['records'] as List).map((r) {
+      return ObjectInfo.fromJson(r as Map<String, dynamic>);
+    }).toList();
+  }
+}
+
+class ObjectInfo {
+  String primaryImageUrl, copyright, description, date, title;
+  String style, century, classification, medium, technique, caption;
+  List<ImageInfo> images;
+  List<PersonInfo> people;
+
+  ObjectInfo.fromJson(Map<String, dynamic> map) {
+    this.primaryImageUrl = map['primaryimageurl'] as String;
+    this.date = map['dated'] as String;
+    this.title = map['title'] as String;
+    this.description = map['description'] as String;
+    this.style = map['style'] as String;
+    this.century = map['century'] as String;
+    this.classification = map['classification'] as String;
+    this.medium = map['medium'] as String;
+    this.technique = map['technique'] as String;
+    this.copyright = (map['copyright'] ?? 'No Copyright') as String;
+    this.caption = map['caption'] as String;
+    this.images = (map['images'] as List<dynamic>)
+        ?.map((h) => ImageInfo.fromJson(h as Map<String, dynamic>))
+        ?.toList();
+    this.people = (map['people'] as List<dynamic>)
+        ?.map((h) => PersonInfo.fromJson(h as Map<String, dynamic>))
+        ?.toList();
+  }
+}
+
+class ImageInfo {
+  String baseImageUrl;
+  int width;
+  int height;
+
+  ImageInfo.fromJson(Map<String, dynamic> map) {
+    this.baseImageUrl = map['baseimageurl'] as String;
+    this.width = map['width'] as int;
+    this.height = map['height'] as int;
+  }
+}
+
+class PersonInfo {
+  String name, role, lifeSpan, gender, birthPlace, deathPlace, culture;
+
+  PersonInfo.fromJson(Map<String, dynamic> map) {
+    this.name = map['displayname'] as String;
+    this.role = map['role'] as String;
+    this.lifeSpan = map['displaydate'] as String;
+    this.gender = map['gender'] as String;
+    this.culture = map['culture'] as String;
+    this.birthPlace = (map['birthplace'] ?? 'Unknown') as String;
+    this.deathPlace = (map['deathplace'] ?? 'Unknown') as String;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,112 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(child: Text('Empty Shell for Art Museum')),
+      theme: ThemeData(primaryColor: Colors.teal[50]),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<ObjectInfo> picData;
+
+  void initState() {
+    super.initState();
+    requestImages().then((images) {
+      setState(() => picData = images);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal[100],
+      appBar: AppBar(
+        title: Text('The Art Museum'),
+        backgroundColor: Colors.teal[50],
+      ),
+      body: Column(
+        children: [
+          Expanded(child: worksOfArt()),
+          RaisedButton(
+            child: Text('Load more Artwork'),
+            onPressed: () async {
+              final images = await requestImages();
+              setState(() => picData.addAll(images));
+            },
+            color: Colors.white,
+            splashColor: Colors.brown,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget worksOfArt() {
+    final deviceSize = MediaQuery.of(context).size;
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: (picData == null) ? 0 : picData.length,
+      itemBuilder: (context, index) {
+        var data = picData[index];
+        var imageData = picData[index].images[0];
+        var height, width;
+        if (imageData.height / 3 > deviceSize.height - 245) {
+          height = deviceSize.height / 1.5;
+          var scaleRatio = (deviceSize.height / 1.5) / imageData.height;
+          width = imageData.width * scaleRatio;
+        } else {
+          height = imageData.height / 3;
+          width = imageData.width / 3;
+        }
+        return Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: InkWell(
+                  onTap: () => null,
+                  child: Hero(
+                    tag: data.primaryImageUrl,
+                    child: FadeInImage.assetNetwork(
+                      height: height,
+                      width: width,
+                      fadeInDuration: const Duration(milliseconds: 500),
+                      placeholder: 'assets/flutterLogo.jpg',
+                      placeholderScale: 4,
+                      image: data.primaryImageUrl,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 10.0,
+                      spreadRadius: 3,
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Colors.brown,
+                    width: 10.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
